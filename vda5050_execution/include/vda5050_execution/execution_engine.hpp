ed4243c98b2ffb6304e69d7fca00c676a730fed4
Saurabh Kamat <saurabh_kamat@a-star.edu.sg>
2026-01-07 15:23:28 +0800

    feat: engine to process events and send to appropriate callbacks
    Signed-off-by: Saurabh Kamat <saurabh_kamat@a-star.edu.sg>

---
+++ vda5050_execution/include/vda5050_execution/execution_engine.hpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2026 ROS-Industrial Consortium Asia Pacific
+ * Advanced Remanufacturing and Technology Centre
+ * A*STAR Research Entities (Co. Registration No. 199702110H)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VDA5050_EXECUTION__EXECUTION_ENGINE_HPP_
+#define VDA5050_EXECUTION__EXECUTION_ENGINE_HPP_
+
+#include <functional>
+#include <memory>
+
+#include "vda5050_execution/callback_registry.hpp"
+#include "vda5050_execution/event_queue.hpp"
+
+namespace vda5050_execution {
+
+class ExecutionEngine
+{
+public:
+  template <typename EventT, typename... Args>
+  void emit(Args&... args)
+  {
+    event_queue_.push<EventT>(std::forward<Args>(args)...);
+  }
+
+  template <typename EventT>
+  void on(std::function<void(std::shared_ptr<EventT>)> callback)
+  {
+    callback_registry_.register_callback<EventT>(std::move(callback));
+  }
+
+  void step();
+
+private:
+  EventQueue event_queue_;
+  CallbackRegistry callback_registry_;
+};
+
+}  // namespace vda5050_execution
+
+#endif  // VDA5050_EXECUTION__EXECUTION_ENGINE_HPP_
---
+++ vda5050_execution/src/vda5050_execution/execution_engine.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2026 ROS-Industrial Consortium Asia Pacific
+ * Advanced Remanufacturing and Technology Centre
+ * A*STAR Research Entities (Co. Registration No. 199702110H)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "vda5050_execution/execution_engine.hpp"
+#include "vda5050_execution/event.hpp"
+
+namespace vda5050_execution {
+
+void ExecutionEngine::step()
+{
+  if (!event_queue_.empty())
+  {
+    std::shared_ptr<EventBase> event = event_queue_.pop();
+    callback_registry_.dispatch(event);
+  }
+}
+
+}  // namespace vda5050_execution
